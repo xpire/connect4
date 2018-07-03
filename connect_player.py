81db3664d8951c2ab35e29722056b128e0a963b1
Justin <xpirep@gmail.com>
2018-07-03 23:14:19 +1000

    making some small changes and adding connect_player.py

--- connect_player.py
+++ connect_player.py
@@ -1,3 +1,70 @@
+#given a board, we will compute using DeepLearning or AlphaBeta pruning of
+#Minimax Search, to compute the best move, and return it.
+#game mechanics and variables
+ncol = 7
+nrow = 6
+connect = 4
+
+#compute next move
+def compute(board):
+    #extract winning information from board
+
+
+#player code
+"""
+def update_value(self):
+    #check the state of 0s (any 4s, 3s, 2s and 1s that could potentially be winning)
+    #1 check vertical
+    i = 0
+    print("VERTICAL")
+    for k in range(ncol): #vary x
+        for j in range(0,nrow-3): #vary y
+            print("checking 4 in a row {} ({},{}) to ({},{})".format(i,k,j,k,j+3))
+            #check
+            connect_n = []
+            for l in range(connect):
+                connect_n.append(self.board[j+l][k])
+            val = check(connect_n)
+            print("connect_n:{}, val:{}".format(connect_n,val))
+            i+=1
+    #2 check horizontal
+    print("HORIZONTAL")
+    for j in range(nrow): #vary y
+        for k in range(0,ncol-3): #vary x
+            print("checking 4 in a row {} ({},{}) to ({},{})".format(i,k,j,k+3,j))
+            #check
+            connect_n = []
+            for l in range(connect):
+                connect_n.append(self.board[j][k+l])
+            val = check(connect_n)
+            print("connect_n:{}, val:{}".format(connect_n,val))
+            i+=1
+    #3 check diagonals
+    print("DIAGONALS")
+    for j in range(0,nrow-3): #vary y
+        for k in range(0,ncol-3): #vary x
+            print("checking 4 in a row first {} ({},{}) to ({},{})".format(i,k,j,k+3,j+3))
+            #check
+            connect_n = []
+            for l in range(connect):
+                connect_n.append(self.board[j+l][k+l])
+            val = check(connect_n)
+            print("connect_n:{}, val:{}".format(connect_n,val))
+            i+=1
+            print("checking 4 in a row second {} ({},{}) to ({},{})".format(i,k,j+3,k+3,j))
+            #check
+            connect_n = []
+            for l in range(connect):
+                connect_n.append(self.board[j+connect-1-l][k+l])
+            val = check(connect_n)
+            print("connect_n:{}, val:{}".format(connect_n,val))
+            i+=1
+    return
+"""
+
+
+"""
+old:
 import random
 import sys, time
 import itertools as it
@@ -180,3 +247,4 @@ if __name__ == "__main__":
         g.player = (g.player+1)%2
         g.update_value()
     print("The game ended with {}, player {} won!".format(g.value, 0.5*g.value + 0.5))
+"""
